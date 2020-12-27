964e897444a8599350ba98fa39d2cffdf1cff865
Jean-Marc van Leerdam <jean-marc.van.leerdam@ordina.nl>
2020-12-27 15:35:18 +0100

    Prepare for writing the output

--- src/main/scala/nl/codestar/sbt/plugin/azurefunctions/FunctionConfigGenerator.scala
+++ src/main/scala/nl/codestar/sbt/plugin/azurefunctions/FunctionConfigGenerator.scala
@@ -3,6 +3,7 @@ package nl.codestar.sbt.plugin.azurefunctions
 import com.microsoft.azure.common.function.configurations.FunctionConfiguration
 import com.microsoft.azure.common.function.handlers.AnnotationHandlerImpl
 
+import java.io.File
 import java.lang.reflect.Method
 import java.net.URL
 import collection.JavaConverters._
@@ -17,5 +18,11 @@ object FunctionConfigGenerator {
   def getConfigs(urls: List[URL]): Map[String, FunctionConfiguration] = {
     val functions = getFunctions(urls)
     handler.generateConfigurations(functions.asJava).asScala.toMap
+  }
+
+  def generateFunctionJsons( jarName: String, baseFolder: File, configs: Map[String, FunctionConfiguration]): Unit = {
+    //TODO: ensure baseFolder exists
+    //TODO: add ScriptFilePath to the configs (fill it with '../$jarName.jar')
+    //TODO: for each K->V in map write function.json to folder K, using values from V
   }
 }
